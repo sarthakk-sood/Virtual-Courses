3a525118a8fec56275139f115757937b72d226fe
Sarthak Sood <142911705+aslistk@users.noreply.github.com>
2026-03-01 18:13:35 +0530

    Fixed case sensitive imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
